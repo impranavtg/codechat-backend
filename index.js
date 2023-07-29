e94df14021f709b2b322e28f2ebec53caacb4ec3
Pranav Tyagi <83161133+impranavtg@users.noreply.github.com>
2023-07-29 23:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const server=app.listen(port, () => {
   const io=require("socket.io")(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://codechat-frontend-bgcvnqjzd-impranavtg.vercel.app/",
     }
   });
 
